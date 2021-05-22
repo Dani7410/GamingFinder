ca7a46edd66f0061155489c774ac938a95bebb13
ironfirst <Daniel_petersen_99@msn.com>
2021-05-22 14:02:37 +0200

    Oprettet routes med mongoAtlastCloud samt git ignore på -env

---
+++ routes/MongoAtlastCloud.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient;
+		
+		
+			const uri = "mongodb+srv://admin1:<password>@cluster0.ktewt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+		
+		
+			const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+		
+		
+			client.connect(err => {
+		
+		
+			const collection = client.db("test").collection("devices");
+		
+		
+			// perform actions on the collection object
+		
+		
+			client.close();
+		
+		
+			});
