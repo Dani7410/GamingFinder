c8f8db20bb18cd966f6e4c067023fd5e29d0285e
ironfirst <Daniel_petersen_99@msn.com>
2021-05-22 14:15:51 +0200

    Mongo Db Atlast Cloud service connection route oprettet

--- routes/MongoAtlastCloud.js
+++ routes/MongoAtlastCloud.js
@@ -1,9 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
 		
 		
-			const uri = "mongodb+srv://admin1:<password>@cluster0.ktewt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+			const uri = "mongodb+srv://"+process.env.USER+":<"+process.env.PASS+">@cluster0.ktewt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 		
-		
+                                                                        // der kan være nogle complicationer med unifiedTopology evt false. svært check at finde nævnte han.
 			const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 		
 		
