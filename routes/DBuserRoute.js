5eda02bfb912de512e46f6444a18b717bfd82040
ironfirst <Daniel_petersen_99@msn.com>
2021-05-27 13:23:28 +0200

    small changes loginPost

--- routes/DBuserRoute.js
+++ routes/DBuserRoute.js
@@ -24,7 +24,20 @@ router.post("/login", (req, res)=>{
     const _matchName = req.body.accountName
     const _matchPw = req.body.accountPassword
 
-    User.find
+    User.findOne({
+        arrountName : _matchName
+    }).then((error, user) => {
+        if(error){
+            res.status(500).send({ message: error });
+            return;
+        }
+        
+            
+        
+    })
+      .catch((error) => {
+        console.log(error);
+      });
 }) 
 
 router.get('/Oneuser/:id', (req, res) =>{
